12bb53c197d114ce59aa9f12dd4519dd1a5a0d0b
Jeremy Faden <faden@cottagesystems.com>
2019-12-23 07:23:01 -0600

    Update whyNoSyntaxHighlite.py

--- bugs/2019/20191220/whyNoSyntaxHighlite.py
+++ bugs/2019/20191220/whyNoSyntaxHighlite.py
@@ -1,6 +1,12 @@
-def foo(s):
-   print(len(s))
+class Foo:
+   def __init__(self,prefix):
+       self.prefix = prefix
+         
+   def foo(s):
+       print(prefix + str(len(s)))
 
 ss = '1234567890'
+myfoo= Foo('>>')
+
 for i in range(5,10):
-   foo( ss[0:i] )
+   myfoo.foo( ss[0:i] )
