9458ad8fdeeb0d004ad735490e0fbb0cc297c983
Jeremy Faden <faden@cottagesystems.com>
2019-12-23 07:24:41 -0600

    Update whyNoSyntaxHighlite.py

--- bugs/2019/20191220/whyNoSyntaxHighlite.py
+++ bugs/2019/20191220/whyNoSyntaxHighlite.py
@@ -1,12 +1,13 @@
 class Foo:
-   def __init__(self,prefix):
-       self.prefix = prefix
+    def __init__(self,prefix):
+        self.prefix = prefix
          
-   def foo(self,s):
-       print(self.prefix + str(len(s)))
+    def foo(self,s):
+        'print the length of the string with the prefix.'
+        print(self.prefix + str(len(s)))
 
 ss = '1234567890'
 myfoo= Foo('>>')
 
 for i in range(5,10):
-   myfoo.foo( ss[0:i] )
+    myfoo.foo( ss[0:i] )
