d1ca7b9e0e98c44b2816dc95009a416e983eacfe
jbfaden <faden@cottagesystems.com>
2020-05-24 08:13:55 -0500

    experiment with Groovy

---
+++ demos/2019/20190424/FirstAutoplot.groovy
@@ -0,0 +1,6 @@
+dom.plots[0].title='Hello From Groovy'
+sleep(1000)
+dom.plots[0].title='hello from groovy'
+sleep(1000)
+dom.plots[0].title='Hello from Groovy'
+// I don't think I can import Autoplot's ScriptContext because it's on different class loader.
